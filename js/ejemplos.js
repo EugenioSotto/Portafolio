975911aa359391d4d87fb012ffbf692032d68fa0
EugenioSoto <EugenioSotto@gmail.com>
2019-10-12 13:24:20 -0500

    Burger Buttom it's now full operationalyty ish

---
+++ js/ejemplos.js
@@ -0,0 +1,62 @@
+// var Menor = parseInt(prompt("¿Cuál es el número menor?"))
+
+// var Mayor = parseInt(prompt("¿Cuál es el número mayor?"))
+
+// var Orden = parseInt(prompt("Si lo quieres descendente, escribe 1. Si lo quieres ascendente escibre 2."))
+
+
+// if (Orden == 1) {
+//     for (var i=Menor; i <= Mayor; i++ ) {
+//         console.log (i)
+//     }
+// } else if (Orden == 2) {
+//     for (var i = Mayor; i >= Menor; i--) {
+//         console.log (i)
+//     }
+// } else {
+//     alert("Algo pasó mal")
+// }
+
+// var Dinero = parseInt(prompt("¿Cuánto dinero tienes?"))
+
+// if (Dinero == 0) {
+//     console.log ("No tienes dinero")
+// } else if (Dinero >= 1 && Dinero <= 50) {
+//     console.log ("Puedes comprar unas papas y un chesquito")
+// } else if (Dinero > 50 && Dinero <= 200) {
+//     console.log ("Puedes invitar la peda")
+// } else if (Dinero >= 201) {
+//     console.log ("Pinche rico")
+// }
+
+// var days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+
+// for (let i = 0; i <= 4; i++ ) {
+//     console.log (days[i])
+// }
+
+// var savings = [30, 50, 15, 60, 100]
+
+// for (let i = 4; i >= 0; i-- ) {
+//     console.log (savings[i])
+// }
+
+// var car = {
+//     marca: "Mazda",
+//     modelo: "cx30",
+//     color: "amarillo",
+//     precio: 400000,
+//     deUso: false, 
+// }
+
+// var estado
+// if (car.deUso = false) {
+//     estado = "nuevo"
+// } else if (car.deUso = true) {
+//     estado = "usado"
+// }
+
+// console.log (`El ${car.marca} ${car.modelo} de color ${car.color} cuesta ${car.precio} y es ${estado}`) 
+
+
+
